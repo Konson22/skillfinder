2391b55b94eb14d89c3987bf014409f48850d8e1
Konson Ak <konakech3@gmail.com>
2023-09-24 12:00:25 +0300

    corrected backend url endpoint

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  // baseURL:'https://hope4child-api.onrender.com',
-  baseURL:'http://localhost:3001',
+  baseURL:'https://hope4child-api.onrender.com',
+  // baseURL:'http://localhost:3001',
 })
 export default axiosInstance
 
